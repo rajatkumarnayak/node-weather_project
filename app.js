2f4b114e712e721784827cf88d00cf8a3f6e1274
Rajat Nayak <rajat.nayak@ingrammicro.com>
2019-11-08 18:27:36 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const getNotes=require('./notes.js')
+const chalk=require('chalk')
+const yargs=require('yargs')
+
+yargs.version('1.1.8')
+
+yargs.command({
+    command:'add',
+    define:'Adding  notes !',
+    handler:function(){
+        console.log('Adding a new notes!')
+    }
+})
+
+yargs.command({
+    command:'List',
+    define:'Adding  List !',
+    builder:{
+        title:{
+         define:'Test 1',
+         demandOption:true,
+         type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log(argv.title);
+    }
+})
+
+args.parse()
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log('Nayak')
+
+const name ='Alex'
+
+const add= function(a,b){
+    return a+b;
+}
+
+module.exports=add;
